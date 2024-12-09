835da63de845c0a8e1ca130e29de50ec3d36861b
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2024-12-09 17:30:54 +0300

    optimization of querying

--- app/index.php
+++ app/index.php
@@ -3,7 +3,4 @@
 declare(strict_types=1);
 
 require 'vendor/autoload.php';
-
-$redis = new Redis();
-
-$redis->connect('redis', 6379);
+require 'src/view/home.php';
---
+++ app/src/view/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+
+<body>
+    <div class="home">
+        <input type="text" placeholder="type in to search for a word">
+        <button class="home-btn">search</button>
+    </div>
+</body>
+<script defer src="src/scripts/search.js"></script>
+
+</html>
